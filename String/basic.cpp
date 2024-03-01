937737c6e3e0f1750cde531d9bc1b20e277e15d8
rachitkatariyaa <raachitkumar2953@gmail.com>
2024-03-01 21:15:40 +0530

    new file added

---
+++ String/basic.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+
+    // dumb way
+
+    /* char arr[] = {'h', 'e', 'l', 'l', 'o'};
+    for(int i=0;i<5;i++) {
+        cout<<arr[i];
+    } */
+
+
+    // by user 
+
+
+    /* char arr[100];
+    cin>>arr;
+    cout<<arr; */
+
+    // space can not be added in this
+
+
+    // string s = "rachit";
+    // cout<<s;
+
+    string s;
+    cin>>s;
+    cout<<s;
+
+
+
+}
+
